aba39a3b720c483e57762e100353e08eaeaf4d9c
Meet <mathnmachines89@gmail.com>
2024-12-02 00:35:50 -0500

    modify config.ps1 to add echo statements for environment name

--- config.ps1
+++ config.ps1
@@ -1,37 +1,37 @@
 # Define global variables
 $global:PROJECT_NAME = "ml-boilerplate"
-$global:PYTHON_VERSION = "3.12"
-$global:PYTHON_INTERPRETER = "python"
+$global:python_VERSION = "3.12"
+$global:python_INTERPRETER = "python"
 
 # Set up python interpreter environment
 function create_environment {
     if (-Not (Test-Path -Path ".$global:PROJECT_NAME")) {
-        & $global:PYTHON_INTERPRETER -m venv ".$global:PROJECT_NAME"
-        Write-Output ">>> python sandbox environment created."
+        & $global:python_INTERPRETER -m venv ".$global:PROJECT_NAME"
+        Write-Output ">>> python sandbox environment: .$global:PROJECT_NAME created."
     } else {
-        Write-Output ">>> python sandbox environment already exists"
+        Write-Output ">>> python sandbox environment: .$global:PROJECT_NAME already exists"
     }
-    Write-Output ">>> Activate python sandbox environment using command: activate_environment"
+    Write-Output ">>> Activate python sandbox environment: $global:PROJECT_NAME using command: activate_environment"
 }
 
 # Activate python interpreter environment
 function activate_environment {
-    Write-Output ">>> Activating python sandbox environment"
+    Write-Output ">>> Activating python sandbox environment: $global:PROJECT_NAME"
     try {
         & ".$global:PROJECT_NAME/Scripts/Activate.ps1"
-        Write-Output ">>> Activated python sandbox environment"
+        Write-Output ">>> Activated python sandbox environment: .$global:PROJECT_NAME"
         Write-Output ">>> Run 'deactivate' to exit the environment"
     } catch {
-        Write-Output ">>> Error: Failed to activate python sandbox environment"
+        Write-Output ">>> Error: Failed to activate python sandbox environment: .$global:PROJECT_NAME"
         Write-Output $_.Exception.Message
     }
 }
 
-# Install Python Dependencies
+# Install python Dependencies
 function install_requirements {
     try {
-        & $global:PYTHON_INTERPRETER -m pip install -r dev-requirements.txt
-        Write-Output ">>> Python dependencies installed successfully"
+        & $global:python_INTERPRETER -m pip install -r requirements.txt
+        Write-Output ">>> python dependencies installed successfully"
     } catch {
         Write-Output ">>> Error: Failed to install python dependencies"
         Write-Output $_.Exception.Message
