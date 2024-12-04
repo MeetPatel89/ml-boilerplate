cd17158f230bfaed33f7c7fa5ca56cc2d58d32d9
Meet <mathnmachines89@gmail.com>
2024-12-03 22:26:00 -0500

    build project using pyproject.toml

--- ml_boilerplate_module/calculator/__init__.py
+++ ml_boilerplate_module/calculator/__init__.py
@@ -1,3 +1,4 @@
 from .calculator import add, divide, multiply, subtract  # noqa: F401
+from .second import myfunc  # noqa: F401
 
-__all__ = ["add", "divide", "multiply", "subtract"]
+__all__ = ["add", "divide", "multiply", "subtract", "myfunc"]
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="calculator",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=["pandas", "mypy", "black", "flake8", "isort"],
-)
